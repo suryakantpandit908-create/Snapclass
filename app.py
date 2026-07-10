d3b1f9accddefbea525a111478cdaf3ed0e3f2bf
suryakantpandit908-create <suryakantpandit908@gmail.com>
2026-07-10 08:17:43 +0530

    Finishing Touches

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
     st.set_page_config(
-        page_title='SnapClass - Making Attendance faster using AI'
+        page_title='SnapClass - Making Attendance faster using AI',
         page_icon="https://i.ibb.co/YTYGn5qV/logo.png"
     )
     
